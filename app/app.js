189a1516327e1588238b043234ed2c8347e113ad
sssaba <saba.hassan7@gmail.com>
2015-09-01 01:18:00 +0530

    sending list to user of online users

--- app/app.js
+++ app/app.js
@@ -1,12 +1,38 @@
 (function () {
 
 	var App = Ember.Application.create();
+	var SOCKET_SERVER_ADDR = 'http://localhost:5000';
+	
 	window.App = App;
 
-	var socket = io.connect('http://localhost:5000');
-	App.socket = socket;
-
 	App.Router.map(function () {
+		this.route("index", {path : "/"});
+	});
 
+	App.IndexController = Ember.Controller.extend({
+		name : "",
+		userid : "",
+		actions : {
+			submitForm : function () {
+				if(this.get('userid') === "")
+					return;
+
+				var user_details = {name: this.get('name'), userid : this.get('userid')},
+					socket = io.connect(SOCKET_SERVER_ADDR, {forceNew : true}),
+					_this = this;
+				App.socket = socket;
+
+				socket.on('connect', this.handleConnection(socket, user_details));
+			}
+		},
+		handleConnection : function (socket, user_details) {
+			var _this = this;
+			return function () {
+				socket.emit('registeruser', user_details);
+				socket.on('online_user_list', function (data) {
+					// Set user list here
+				});
+			};
+		}
 	});
 })();
--- node-server/lib/connector.js
+++ node-server/lib/connector.js
@@ -1,14 +1,39 @@
 (function () {
 
+	var activeSockets = {};
+
 	var connector = {
 		createChannel : function (io) {
 			this.io = io;
-			io.of("/messenger.html").on('connection', this.handleConnection(io));
+			io.on('connection', this.handleConnection(io));
 		},
 		handleConnection : function (io) {
+			var _this = this;
 			return function (socket) {
-				console.log("Connection message received");
+				if(!activeSockets[socket.id]) {
+					activeSockets[socket.id] = {};
+					activeSockets[socket.id]['socket'] = socket;
+					_this.registerEvents(socket);
+				}
 			};
+		},
+		registerEvents : function (socket) {
+			console.log(socket.id);
+			socket.on('registeruser', function (user_details) {
+				
+				console.log(user_details);
+				activeSockets[socket.id]['user_details'] = user_details;
+				
+				var users = [];
+				Object.keys(activeSockets).forEach(function (key) {
+					if(key !== socket.id) {
+						users.push(activeSockets[key]['user_details']);
+					}
+				});
+				console.log(users);
+				
+				socket.emit('online_user_list', users);
+			});
 		}
 	};
 
